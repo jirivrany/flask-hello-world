80a7846aee1201243d76c9d4f97525d38c46459c
Jiri Vrany <jiri.vrany@cesnet.cz>
2025-02-06 12:22:12 +0100

    proxy fix

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import os
 from flask import Flask, request, render_template
 from dotenv import load_dotenv
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 load_dotenv()  # take environment variables from .env.
 
 app = Flask(__name__)
+# we are behind a proxy, so we need to tell Flask to trust the X-Forwarded- headers
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 
 @app.route("/")
