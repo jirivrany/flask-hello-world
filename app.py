2b46635cc422c8d7624422f53c5e6126fbbb3349
Jiri Vrany <jiri.vrany@cesnet.cz>
2025-02-06 12:11:03 +0100

    add template

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, render_template
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
@@ -12,5 +12,5 @@ def hello_world():
     user = os.getenv("DB_USER", "Not Found")
     remote_addr = request.remote_addr
     cookies = request.cookies
-    cookies_str = ", ".join([f"{k}: {v}" for k, v in cookies.items()])
-    return f"<p>Hello, World! And hello {user} from {remote_addr}</p><p>Cookies: {cookies_str}</p>"
+    headers = request.headers
+    return render_template("debug.html", user=user, remote_addr=remote_addr, cookies=cookies, headers=headers)
